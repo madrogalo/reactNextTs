de7e320ed86178d47ddcbcae5deadba019c0f054
madrogalo <youn4ik@gmail.com>
2021-07-24 17:22:14 +0300

    debbug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
+import React from 'react';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Top next</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
